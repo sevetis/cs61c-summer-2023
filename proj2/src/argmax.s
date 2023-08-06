013f9ca442d16ae22b8886abd33bdc7a8f1fd754
seven <Seve_tis@proton.me>
2023-08-06 12:26:28 +0800

    proj2: complete task 3

--- proj2/src/argmax.s
+++ proj2/src/argmax.s
@@ -17,14 +17,30 @@
 argmax:
     # Prologue
 
+    addi t0 x0 1
+    blt a1 t0 exception
+    lw t1 0(a0)   # t1 = a[0]
+    li t3 0  # t3 = 0
 
 loop_start:
-
+    addi t0 t0 1
+    addi a0 a0 4
+    blt a1 t0 loop_end
+    lw t2 0(a0)   # t2 = *(a + 1)
+    blt t1 t2 loop_continue # t1 = max(t1, t2)
+    j loop_start
 
 loop_continue:
+    mv t1 t2
+    addi t3 t0 -1 # t3 = current_index
+    j loop_start
 
+exception:
+    li a0 36
+    j exit
 
 loop_end:
+    mv a0 t3
     # Epilogue
 
     jr ra
