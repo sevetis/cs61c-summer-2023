bbc0373234535959d21e0a737f16c9873a02e915
seven <Seve_tis@proton.me>
2023-08-06 09:42:38 +0800

    proj2: complete task 2

--- proj2/src/relu.s
+++ proj2/src/relu.s
@@ -14,25 +14,34 @@
 # ==============================================================================
 relu:
     # Prologue
+    addi sp sp -4
+    sw a0 0(sp)
 
+    addi t0 x0 1            # i = 1
+    blt a1 t0 exception     # len < 1 exception
 
 loop_start:
-
-
-
-
-
-
-
+    blt a1 t0 loop_end      # len < i end
+    lw t1 0(a0)             # t1 = a[i-1]
+    addi t0 t0 1            # i++
+    blt t1 zero loop_continue   # if t1 < 0, loop_continue
+    addi a0 a0 4            # a0 += 4
+    j loop_start            
 
 loop_continue:
+    add t1 x0 x0            # t1 = 0
+    sw t1 0(a0)             # set to zero
+    addi a0 a0 4            # next item
+    j loop_start
 
-
-
+exception:
+    li a0 36                # error code 36
+    j exit
 loop_end:
 
 
     # Epilogue
-
+    lw a0 0(sp)
+    addi sp sp 4
 
     jr ra
