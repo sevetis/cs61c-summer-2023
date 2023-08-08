43a24524b146f45dd9433e425377ec1ef59ed53c
seven <Seve_tis@proton.me>
2023-08-08 10:01:24 +0800

    proj2: get stuck on task 7

--- proj2/src/read_matrix.s
+++ proj2/src/read_matrix.s
@@ -27,16 +27,80 @@
 read_matrix:
 
     # Prologue
+    addi sp sp -12
+    sw ra 0(sp)
+    sw a1 4(sp)
+    sw a2 8(sp)
 
+    # fopen
+    li a1 1
+    jal fopen
 
+    li t0 -1
+    beq t0 a0 fopen_failed
 
+    addi sp sp -4
+    sw a0 0(sp)
 
+    # read row and col
+    li a2 4 # read 1 int (row)
+    lw a1 8(sp)
+    jal fread
+    li a2 4
+    bne a0 a2 fread_failed
 
+    lw a1 12(sp)
+    lw a0 0(sp)
+    jal fread
+    li a2 4
+    bne a0 a2 fread_failed
 
+    # alloc space for elements
+    lw a1 8(sp)
+    lw a2 12(sp)
+    lw t0 0(a1)
+    lw t1 0(a2)
+    mul a0 t0 t1
+    addi sp sp -4
+    sw a0 0(sp)
+    jal malloc
+    beq a0 x0 malloc_failed
+    
+    # read elements
+    lw a2 0(sp)
+    mv a1 a0
+    lw a0 4(sp)
+    jal fread
+    lw a2 0(sp)
+    bne a0 a2 fread_failed
+    sw a1 0(sp)
 
+    # fclose
+    lw a0 4(sp)
+    jal fclose
+    bne a0 x0 fclose_failed
 
 
     # Epilogue
-
-
+    lw a0 0(sp)
+    lw ra 8(sp)
+    addi sp sp 20
+    
     jr ra
+
+
+malloc_failed:
+    li a0 26
+    j exit
+
+fopen_failed:
+    li a0 27
+    j exit
+
+fclose_failed:
+    li a0 28
+    j exit
+
+fread_failed:
+    li a0 29
+    j exit
