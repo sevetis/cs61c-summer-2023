be7e4f9347d8a40bc26296506a57a6eec8e5e00e
seven <Seve_tis@proton.me>
2023-08-09 13:07:20 +0800

    proj2: complete task 8

--- proj2/src/write_matrix.s
+++ proj2/src/write_matrix.s
@@ -25,16 +25,86 @@
 write_matrix:
 
     # Prologue
+    addi sp sp -4
+    sw ra 0(sp)
 
+    # OPEN FILE
+    addi sp sp -12
+    sw a1 0(sp)
+    sw a2 4(sp)
+    sw a3 8(sp)
 
+    li a1 1
 
+    jal fopen
 
+    li t0 -1
+    beq a0 t0 fopen_failed
 
+    lw a3 8(sp)
+    lw a2 4(sp)
+    lw a1 0(sp)
+    addi sp sp 12
 
+    # WRITE ROW AND COL
+    addi sp sp -16
+    sw a2 0(sp)
+    sw a3 4(sp)
+    sw a1 8(sp)
+    sw a0 12(sp)
 
+    mv a1 sp
+    li a2 2 # n = 2
+    li a3 4 # size = 4
+
+    jal fwrite
+
+    li a2 2
+    bne a0 a2 fwrite_failed
+
+    lw a0 12(sp)
+    lw a1 8(sp)
+    lw a3 4(sp)
+    lw a2 0(sp)
+    addi sp sp 16
+
+    # WRITE ELEMENTS
+    mul a2 a2 a3 
+
+    addi sp sp -8
+    sw a0 0(sp)
+    sw a2 4(sp)
+
+    li a3 4
+
+    jal fwrite
+
+    lw a2 4(sp)
+    bne a0 a2 fwrite_failed
+
+    lw a0 0(sp)
+    addi sp sp 8
+
+    # CLOSE FILE
+    jal fclose
+    bne a0 x0 fclose_failed
 
 
     # Epilogue
-
+    lw ra 0(sp)
+    addi sp sp 4
 
     jr ra
+
+
+fopen_failed:
+    li a0 27
+    j exit
+
+fclose_failed:
+    li a0 28
+    j exit
+
+fwrite_failed:
+    li a0 30
+    j exit
