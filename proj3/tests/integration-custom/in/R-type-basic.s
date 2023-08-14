319a9269e9955bcee3100c8aefa2827ab0d95858
seven <Seve_tis@proton.me>
2023-08-14 09:28:16 +0800

    proj3: write and pass R-type tests

---
+++ proj3/tests/integration-custom/in/R-type-basic.s
@@ -0,0 +1,32 @@
+addi t0 x0 1
+addi t1 x0 0
+
+add t0 t0 t0
+add t1 t0 t0
+
+sub t0 t1 t0
+
+and t0 t0 t1
+
+or t0 t0 t0
+
+xor t0 t1 t0
+
+addi t0 x0 4
+addi t1 x0 1
+
+sll t1 t1 t0
+
+srl t1 t1 t0
+
+sra t0 t0 t0
+
+slt t1 t1 t0
+
+addi t1 x0 1
+
+mul t1 t1 t0
+
+mulh t1 t1 t0
+
+mulhu t1 t1 t0
