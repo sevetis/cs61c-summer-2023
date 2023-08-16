ac37a892b8f8999985364f5319f018967be4753a
seven <Seve_tis@proton.me>
2023-08-16 20:52:52 +0800

    complete lab08

--- lab08/ex2.c
+++ lab08/ex2.c
@@ -12,6 +12,12 @@ double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the critical keyword here!
+    #pragma omp parallel for
+        for (int i = 0; i < arr_size; i++) {
+            #pragma omp critical
+            global_sum += x[i] * y[i];
+        }
+
     return global_sum;
 }
 
@@ -20,6 +26,12 @@ double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the reduction keyword here!
+    #pragma omp parallel
+    {
+        #pragma omp for reduction(+:global_sum) 
+        for (int i = 0; i < arr_size; i++) 
+            global_sum += x[i] * y[i];
+    }
     return global_sum;
 }
 
@@ -28,5 +40,25 @@ double dotp_manual_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Do NOT use the `reduction` directive here!
+    #pragma omp parallel
+    {
+        int n = omp_get_num_threads();
+        int thread_id = omp_get_thread_num();
+        int stride = arr_size / n;
+        int start = thread_id * stride;
+        double local_sum = 0.0;
+
+        for (int i = start; i < start + stride; i++) {
+            local_sum += x[i] * y[i];
+        }    
+
+        if (thread_id == n - 1) {
+            for (int i = stride * n; i < arr_size; i++) 
+                local_sum += x[i] * y[i];
+        }
+
+        #pragma omp critical
+        global_sum += local_sum;
+    }
     return global_sum;
 }
