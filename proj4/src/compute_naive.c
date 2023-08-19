8a8928a360ba9af8c1a9c1a507c7408370a25fce
seven <Seve_tis@proton.me>
2023-08-19 16:47:12 +0800

    flip matric

--- proj4/src/compute_naive.c
+++ proj4/src/compute_naive.c
@@ -3,16 +3,34 @@
 // Computes the dot product of vec1 and vec2, both of size n
 int32_t dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
   // TODO: implement dot product of vec1 and vec2, both of size n
-
-  return -1;
+  int32_t res = 0;
+  for (uint32_t i = 0; i < n; i++) {
+    res += vec1[i] * vec2[i];
+  }
+  return res;
 }
 
 // Computes the convolution of two matrices
 int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   // TODO: convolve matrix a and matrix b, and store the resulting matrix in
   // output_matrix
+  if (!a_matrix || !b_matrix || !output_matrix) return -1;
+  uint32_t aLen = a_matrix->cols, bLen = b_matrix->cols;
+  if (!aLen || !bLen) return -1;
+  int32_t* A = a_matrix->data, *B = b_matrix->data;
+  if (!A || !B) return -1;
 
-  return -1;
+  for (int32_t i = 0; i < (bLen>>1); i++) {
+    int32_t temp = B[0];
+    B[0] = B[bLen - i - 1];
+    B[bLen - i - 1] = temp;
+  }
+
+  for (int32_t i = 0; i + bLen <= aLen; i++) {
+    output_matrix[0]->data[i] = dot(bLen, A + i, B);
+  }
+
+  return 0;
 }
 
 // Executes a task
