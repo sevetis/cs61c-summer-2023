df8a7c7c07ecae26e367afb1ca8e23f7338b152f
seven <Seve_tis@proton.me>
2023-08-22 14:25:05 +0800

    SIMD improve dot

--- proj4/src/compute_optimized.c
+++ proj4/src/compute_optimized.c
@@ -6,8 +6,27 @@
 // Computes the dot product of vec1 and vec2, both of size n
 int32_t dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
   // TODO: implement dot product of vec1 and vec2, both of size n
+  int32_t result = 0;
+  __m256i sum_vec = _mm256_setzero_si256();
+  __m256i temp1, temp2;
+  for (uint32_t i = 0; i < n / 8 * 8; i += 8) {
+    temp1 = _mm256_loadu_si256((__m256i *)(vec1 + i));
+    temp2 = _mm256_loadu_si256((__m256i *)(vec2 + i));
 
-  return -1;
+    temp1 = _mm256_mul_epi32(temp1, temp2);
+    sum_vec = _mm256_add_epi32(sum_vec, temp1);
+  }
+  
+  int32_t tmp_arr[8];
+  _mm256_store_epi32((__m256i *)tmp_arr, sum_vec);
+
+  for (int i = 0; i < 8; i++) 
+    result += tmp_arr[i];
+
+  for (uint32_t i = n / 8 * 8; i < n; i++) 
+    result += vec1[i] * vec2[i];
+  
+  return result;
 }
 
 // Computes the convolution of two matrices
